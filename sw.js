b13a5a14572134434a1f108e85134004da7d824a
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 00:10:45 +0300

    Polish ghost and house materials while preserving night mechanics

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '5f3cc764f6';
+const VERSION = '33bd4556bc';
 const PREFIX = "ghostlife-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
