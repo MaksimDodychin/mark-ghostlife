7b041263a07fbb4f2566fdc6c02dba6907fda20d
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 10:00:08 +0300

    👥 Онлайн: настройки комнаты (сколько игроков, во что играем), приглашение друга, ник по умолчанию, честные сообщения при слабом интернете

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'bae9ba7280';
+const VERSION = '7513cc9082';
 const PREFIX = "ghostlife-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
